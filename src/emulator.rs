89c7f9900018a84002c685562e943441657bd7aa
MxDkl <max@dekelnet.com>
2025-09-12 10:44:52 -0700

    starting to implement each instruction

--- src/emulator.rs
+++ src/emulator.rs
@@ -92,7 +92,7 @@ struct UType {    // bits
 impl From<u32> for UType {
     fn from(ins: u32) -> Self {
         UType {
-            rd:      ((ins >> 7) & 0b11111) as u8,
+            rd:      ((ins >> 7)  & 0b11111) as u8,
             imm3112:  (ins >> 12) as u32,
         }
     }
@@ -107,7 +107,7 @@ struct JType {           // bits
 impl From<u32> for JType {
     fn from(ins: u32) -> Self {
         JType { 
-            rd:             ((ins >> 7) & 0b11111) as u8,
+            rd:             ((ins >> 7)  & 0b11111) as u8,
             imm20101111912:  (ins >> 12) as u32,
         }
     }
@@ -126,7 +126,7 @@ impl Interpreter {
     /// Constructor
     pub fn new() -> Self {
         Interpreter {
-            /// 32 registers, 1 program counter
+            /// 32 x-registers, 1 program counter
             registers: vec![0; 33],
 
             ///Virual memory space
@@ -137,10 +137,70 @@ impl Interpreter {
     
     // Private functions
     
-    pub fn instruction_decode(self, ins: u32) -> u8 {
+    pub fn execute_instruction(mut self, ins: u32) -> u8 {
         let opcode = ins & 0b111111;
-        let instruct = IType::from(ins);
-        println!("{:?}", instruct);
+
+        match opcode {
+
+            // LUI - UType
+            0b0110111 => {
+                let ins = UType::from(ins);
+                self.registers[ins.rd as usize] = ins.imm3112 as u64;
+            },
+
+            // AUIPC - UType
+            0b0010111 => {
+                let ins = UType::from(ins);
+            },
+            
+            // JAL - JType
+            0b1101111 => {
+                let ins = JType::from(ins);
+            },
+
+            // JALR - IType
+            0b1100111 => {
+                let ins = IType::from(ins);
+            },
+
+            // BEQ/BNE/BLT/BGE/BLTU/BGEU - BType
+            0b1100011 => {
+                let ins = BType::from(ins);
+            },
+
+            // LB/LH/LW/LBU/LHU - IType
+            0b0000011 => {
+                let ins = IType::from(ins);
+            },
+
+            // SB/SH/SW - SType
+            0b0100011 => {
+                let ins = SType::from(ins);
+            },
+
+            // ADDI/SLTI/SLTIU/XORI/ORI/ANDI/SLLI/SRLI/SRAI- IType
+            0b0010011 => {
+                let ins = IType::from(ins);
+            },
+        
+            // ADD/SUB/SLL/STL/STLU/XOR/SRL/SRA/OR/AND - IType
+            0b0110011 => {
+                let ins = IType::from(ins);
+            },
+
+            // FENCE/FENCE.TSO/PAUSE - IType
+            0b0001111 => {
+                let ins = IType::from(ins);
+            },
+
+            // ECALL/EBREAK - IType
+            0b11100111 => {
+                let ins = IType::from(ins);
+            },
+
+            _ => panic!(),
+
+        }
 
         0
     }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ pub mod mmu;
 
 fn main() {
     let emu = emulator::Interpreter::new();
-    emu.instruction_decode(0b11111111000000010000000100010011u32);
+    emu.execute_instruction(0b11111111000000010000000100010011u32);
 }
